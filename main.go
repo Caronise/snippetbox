b5fc70475aa6c4fa08cbfec249d6e909abee7c72
Lucas <lucas.betancourt@hotmail.com>
2023-10-16 11:21:19 -0500

    Modified and extended the SnippetCreate handler to use http methods and types

--- main.go
+++ main.go
@@ -8,6 +8,15 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path matches "/". if it doesn't, use
+	// the http.NotFound() function to send a 404 response to the client.
+	// Importantly, we then return from the handler. If we don't return the
+	// handler would keep executing.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
@@ -18,6 +27,15 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// If not POST, send a 405 status code and "Method not allowed" response body
+	if r.Method != http.MethodPost {
+		// use the Header().Set() method to indicate what request methods are
+		// supported for this URL.
+		w.Header().Set("Allow", http.MethodPost)
+		// http.Error() calls w.WriteHeader() and w.Write() behind the scenes.
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("Create a new snippet..."))
 }
 
