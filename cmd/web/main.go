16cd79c53f66246b2264b8c64209f1fbfe36cd82
Lucas <lucas.betancourt@hotmail.com>
2023-10-18 08:30:09 -0500

    Added structured logging from log/slog package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -12,13 +13,20 @@ func main() {
 
 	flag.Parse()
 
+	// Initialize a new structured logger, which writes to the Stdout and use
+	// the specified settings.
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		//		Level: slog.LevelDebug,
+	}))
+
 	mux := http.NewServeMux()
 
 	// Create a file server to serve files out of ./ui/static directory.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
-	// Use mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". Then strip "/static" prefix.
+	// Register the file server as the handler for all URL paths that start with
+	// "/static/". Then strip "/static" prefix.
 	// otherwise it will try to access: ./ui/static/static/somefile.css
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
@@ -27,8 +35,9 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting server on ", *addr)
+	logger.Info("Starting server on ", *addr)
 
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
